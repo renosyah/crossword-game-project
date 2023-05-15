670dcdf1ab8c6eebc88cbdadf1c22cd208c5c13e
renosyah <syahputrareno975@gmail.com>
2023-05-15 14:34:14 +0700

    add loading

--- menu/login/ui/ui.gd
+++ menu/login/ui/ui.gd
@@ -1,4 +1,25 @@
 extends Control
 
+@onready var loading = $CanvasLayer/Control/loading
+
+func _ready():
+	OAuth2.sign_in_completed.connect(_sign_in_completed)
+	OAuth2.sign_in_expired.connect(_sign_in_expired)
+	
+	OAuth2.check_sign_in_status()
+	loading.visible = true
+	
+func _sign_in_completed():
+	loading.visible = false
+	get_tree().change_scene_to_file("res://menu/main_menu/main_menu.tscn")
+	
+func _sign_in_expired():
+	loading.visible = false
+	
 func _on_login_pressed():
+	if loading.visible:
+		return
+		
+	loading.visible = true
 	OAuth2.sign_in()
+	
--- menu/main_menu/ui/ui.gd
+++ menu/main_menu/ui/ui.gd
@@ -2,21 +2,23 @@ extends Control
 
 @onready var score = $CanvasLayer/Control/score
 @onready var label = $CanvasLayer/Control/SafeArea/VBoxContainer/HBoxContainer/Label
+@onready var loading = $CanvasLayer/Control/loading
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	score.visible = false
-	label.visible = false
+	loading.visible = true
 	
 	OAuth2.get_profile.connect(_get_profile)
 	OAuth2.sign_out_completed.connect(_sign_out_completed)
 	OAuth2.get_profile_info()
 	
 func _get_profile(profile : OAuth2.OAuth2UserInfo):
-	label.visible = true
+	loading.visible = false
 	label.text = "Welcome : %s" % profile.given_name
 	
 func _sign_out_completed():
+	loading.visible = false
 	get_tree().change_scene_to_file("res://menu/login/login.tscn")
 	
 func _on_play_pressed():
@@ -29,4 +31,12 @@ func _on_score_button_pressed():
 	score.display_score(Global.get_scores())
 
 func _on_sign_out_pressed():
+	if loading.visible:
+		return
+		
+	loading.visible = true
 	OAuth2.sign_out()
+
+
+
+
