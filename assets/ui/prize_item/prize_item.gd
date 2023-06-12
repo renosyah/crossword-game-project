048b8ab1273fc912455aaeaa51ddb782e1bfd127
renosyah <syahputrareno975@gmail.com>
2023-06-12 22:34:04 +0700

    prizes item & menu

---
+++ assets/ui/prize_item/prize_item.gd
@@ -0,0 +1,82 @@
+extends MarginContainer
+class_name PrizeItem
+
+signal redeem(_id)
+
+@export var id :int
+@export var prize_name :String
+@export var prize_image_url :String
+@export var prize_level :int
+
+@export var can_redeem :bool
+
+@onready var _prize_name = $Panel/HBoxContainer/VBoxContainer/prize_name
+@onready var _level = $Panel/HBoxContainer/VBoxContainer/HBoxContainer/level
+@onready var _button_redeem_label = $Panel/HBoxContainer/VBoxContainer/HBoxContainer/redeem/CenterContainer/Label
+@onready var _texture_rect = $Panel/HBoxContainer/TextureRect
+@onready var _redeem_button = $Panel/HBoxContainer/VBoxContainer/HBoxContainer/redeem
+@onready var _http_request = $HTTPRequest
+
+@onready var _panel_style :StyleBoxFlat = $Control/Panel.get_theme_stylebox(StringName("panel")).duplicate()
+@onready var _panel = $Control/Panel
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_panel.remove_theme_stylebox_override("panel")
+	_panel.add_theme_stylebox_override("panel", _panel_style)
+	
+	var rng = RandomNumberGenerator.new()
+	rng.seed = id
+	
+	_panel_style.bg_color = Color(rng.randf(), rng.randf(), rng.randf(), 1.0)
+	_redeem_button.modulate.a = 1 if can_redeem else 0.5
+	
+	_prize_name.text = prize_name
+	_level.text = "%s %s" % [tr("LEVEL") ,prize_level]
+	
+	if prize_image_url.is_empty():
+		return
+		
+	if "Web" == OS.get_name():
+		_http_request.accept_gzip = false
+		
+	var error = _http_request.request(prize_image_url)
+	if error != OK:
+		return
+	
+func _on_redeem_pressed():
+	if not can_redeem:
+		return
+		
+	emit_signal("redeem", id)
+	
+func _on_http_request_request_completed(result: int, _response_code: int, _headers: PackedStringArray, body: PackedByteArray):
+	if result != HTTPRequest.RESULT_SUCCESS:
+		return
+		
+	if body.is_empty():
+		return null
+		
+	var img = _check_image_format(body)
+	if img == null:
+		return null
+		
+	_texture_rect.texture = ImageTexture.create_from_image(img)
+	
+func _check_image_format(body :PackedByteArray) -> Image:
+	var img = Image.new()
+	
+	var image_error = img.load_png_from_buffer(body)
+	if image_error == OK:
+		return img
+		
+	image_error = img.load_jpg_from_buffer(body)
+	if image_error == OK:
+		return img
+		
+	image_error = img.load_bmp_from_buffer(body)
+	if image_error == OK:
+		return img
+		
+	return null
+
--- main/sub_main_scene/prize/prize.gd
+++ main/sub_main_scene/prize/prize.gd
@@ -3,10 +3,22 @@ extends Control
 signal back
 
 @onready var label = $VBoxContainer/HBoxContainer2/Label
+@onready var v_box_container = $VBoxContainer/HBoxContainer/ScrollContainer/VBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	label.text = tr("PRIZE")
-
+	
+func show_prize():
+	_remove_child(v_box_container)
+	
+func _remove_child(node :Control):
+	for i in node.get_children():
+		node.remove_child(i)
+		i.queue_free()
+		
+	node.custom_minimum_size.y = 0
+	
 func _on_back_button_pressed():
 	emit_signal("back")
+
